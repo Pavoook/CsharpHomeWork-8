0259e3ee93baadab1069c379452361905f08ce3b
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-29 06:38:19 +0500

    Corrected Exersize №58 Matrix Multiplication

--- Ex058_MatrixMultiplication/Program.cs
+++ Ex058_MatrixMultiplication/Program.cs
@@ -11,23 +11,26 @@ int minBound = 0;
 int maxBound = 9;
 
 int amountRows1 = 2;
-int amountCols1 = 2;
+int amountCols1 = 3;
 int[,] matrix1 = new int[amountRows1, amountCols1];
 FillRandomMatrix(matrix1, minBound, maxBound);
 Console.WriteLine("Дана матрица 1:");
 PrintMatrix(matrix1);
 
-int amountRows2 = 2;
+int amountRows2 = 3;
 int amountCols2 = 2;
 int[,] matrix2 = new int[amountRows2, amountCols2];
 FillRandomMatrix(matrix2, minBound, maxBound);
 Console.WriteLine("Дана матрица 2:");
 PrintMatrix(matrix2);
 
-int[,] mutiplyMatrix = GetMatrixMultiplication(matrix1, matrix2);
-Console.WriteLine("Произведение этих матриц:");
-PrintMatrix(mutiplyMatrix);
-
+if (ValidateMatrix(matrix1, matrix2))
+{
+    int[,] mutiplyMatrix = GetMatrixMultiplication(matrix1, matrix2);
+    Console.WriteLine("Произведение этих матриц:");
+    PrintMatrix(mutiplyMatrix);
+}
+else Console.WriteLine("Ошибка! Кол-во столбцов в матрице 1 не совпадает с кол-вом строк в матрице 2.");
 
 void FillRandomMatrix(int[,] table, int min, int max)
 {
@@ -51,6 +54,13 @@ void PrintMatrix(int[,] table)
         }
         Console.WriteLine();
     }
+}
+
+bool ValidateMatrix(int[,] table1, int[,] table2)
+{
+    bool result = false;
+    if (table1.GetLength(1) == table2.GetLength(0)) result = true;
+    return result;
 }
 
 int[,] GetMatrixMultiplication(int[,] table1, int[,] table2)
